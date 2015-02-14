d99222095f1d715e834609805b6d2a53c3128401
cjq002 <692771080@qq.com>
2015-02-14 09:16:04 +0800

    Initial Commit

---
+++ 纯代码创建UITabBarController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  纯代码创建UITabBarController
+//
+//  Created by 陈家庆 on 15-2-14.
+//  Copyright (c) 2015年 shikee_Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 纯代码创建UITabBarControllerTests/_____UITabBarControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  _____UITabBarControllerTests.m
+//  纯代码创建UITabBarControllerTests
+//
+//  Created by 陈家庆 on 15-2-14.
+//  Copyright (c) 2015年 shikee_Chan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _____UITabBarControllerTests : XCTestCase
+
+@end
+
+@implementation _____UITabBarControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
